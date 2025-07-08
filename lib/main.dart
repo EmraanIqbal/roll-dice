c988dd51b12e5ebf3d9d7c4d96f2f8675ae4f428
Your Name <your.email@example.com>
2025-07-09 02:23:05 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Text(
+            'Hello World!',
+          ),
+        ),
+      ),
+    ),
+  );
+}
