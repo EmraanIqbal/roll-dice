5c1fec9b49772436fde6e2daa8d7a4bc1180a190
Your Name <your.email@example.com>
2025-07-18 18:06:21 +0500

    added the random number functionality

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,15 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomLeft;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color, {super.key});
+  const GradientContainer(this.color, {super.key});
 
-  GradientContainer.purple(this.color, {super.key});
-
-  var activeDiceImage = 'assets/images/dice-1.png';
+  const GradientContainer.purple(this.color, {super.key});
 
   final List<Color> color;
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-3.png';
-  }
 
   @override
   Widget build(context) {
@@ -27,25 +22,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: Text('Roll Dice'),
-            ),
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
